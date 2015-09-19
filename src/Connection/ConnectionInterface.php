fbf0eb0b27bf85ab7aad269689ee5add8050f1e1
huyanping <hypxm@qq.com>
2015-09-19 10:58:04 +0800

    add ConnectionInterface

---
+++ src/Connection/ConnectionInterface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015/9/19
+ * Time: 10:57
+ */
+
+namespace Nsq\Connection;
+
+
+class ConnectionInterface
+{
+
+
+
+}
--- src/Wire/Reader.php
+++ src/Wire/Reader.php
@@ -11,5 +11,32 @@ namespace Nsq\Wire;
 
 class Reader
 {
+    const FRAME_TYPE_RESPONSE = 0;
+    const FRAME_TYPE_ERROR = 1;
+    const FRAME_TYPE_MESSAGE = 2;
 
+    protected $size;
+
+    protected $type;
+
+    protected $content;
+
+    public function readSize($data, $connection)
+    {
+        if (strlen($data) < 4) {
+            return false;
+        }
+        $size_data = substr($data, 0, 4);
+        $size = unpack("N", $size_data);
+
+        return $size[1];
+    }
+
+    public function readType($data){
+        if(strlen($data) < 8){
+            return false;
+        }
+    }
+
+    protected function readInt($data)
 }
