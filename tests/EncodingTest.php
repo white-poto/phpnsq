70e40c3e45385d3a3de03f966bde137990224647
huyanping <hypxm@qq.com>
2015-09-21 16:21:00 +0800

    tests

--- tests/EncodingTest.php
+++ tests/EncodingTest.php
@@ -29,13 +29,13 @@ class WriterTest extends PHPUnit_Framework_TestCase
         fwrite($socket_client, $this->writer->magic());
         fwrite($socket_client, $this->writer->publish("test", "test"));
         $data = fread($socket_client, 1024);
-        var_dump($data);
         $result = $this->reader->decode($data);
-        var_dump($result);
+        $this->assertTrue($result);
         $this->assertEquals("OK", $this->reader->getContent());
         $this->assertEquals(2, $this->reader->getSize());
         $this->assertEquals(0, $this->reader->getType());
-        sleep(2);
+        sleep(5);
+        fwrite($socket_client, $this->writer->close());
 
     }
 }
