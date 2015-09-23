c726370356290dcfd8d9883dc96bee064e690a18
huyanping <hypxm@qq.com>
2015-09-23 16:46:01 +0800

    debug

--- src/Connection/Connection.php
+++ src/Connection/Connection.php
@@ -58,7 +58,7 @@ class Connection
             throw new ConnectionException($message);
         }
 
-        $connect_result = socket_connect($host, $port);
+        $connect_result = socket_connect($this->socket, $host, $port);
         if ($connect_result === false) {
             $message = "socket_connect failed. reason:"
                 . socket_strerror(socket_last_error());
