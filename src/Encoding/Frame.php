b735ee6546df5fa7becd927096987695b56a5ae6
huyanping <hypxm@qq.com>
2015-09-24 17:53:50 +0800

    add comment

--- src/Encoding/Frame.php
+++ src/Encoding/Frame.php
@@ -11,12 +11,26 @@ namespace Nsq\Encoding;
 
 class Frame
 {
+    /**
+     * @var
+     */
     protected $size;
 
+    /**
+     * @var
+     */
     protected $type;
 
+    /**
+     * @var
+     */
     protected $content;
 
+    /**
+     * @param $size
+     * @param $type
+     * @param $content
+     */
     public function __construct($size, $type, $content)
     {
         $this->size = $size;
@@ -24,21 +38,33 @@ class Frame
         $this->content = $content;
     }
 
+    /**
+     * @return mixed
+     */
     public function getSize()
     {
         return $this->size;
     }
 
+    /**
+     * @return mixed
+     */
     public function getType()
     {
         return $this->type;
     }
 
+    /**
+     * @return mixed
+     */
     public function getContent()
     {
         return $this->content;
     }
 
+    /**
+     * @return Message
+     */
     public function getMessage()
     {
         return new Message($this->content);
