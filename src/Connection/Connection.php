38ee48e3fb175883eba3e60bda22e54ad9b1671e
huyanping <hypxm@qq.com>
2015-09-29 10:48:28 +0800

    add react event loop

--- src/Connection/Connection.php
+++ src/Connection/Connection.php
@@ -13,194 +13,60 @@ use Nsq\Encoding\Buffer;
 use Nsq\Encoding\Decoder;
 use Nsq\Exception\ConnectionException;
 use Nsq\Exception\RuntimeException;
+use Nsq\Lookup\LookupCluster;
+use React\EventLoop\LoopInterface;
 
 class Connection
 {
-
-    const RECONNECT_WAIT_TIME = 4;
+    /**
+     * @var LookupCluster
+     */
+    protected $lookup;
 
     /**
-     * @var string
+     * @var LoopInterface
      */
-    protected $host;
+    protected $loop;
 
-    /**
-     * @var integer
-     */
-    protected $port;
+    protected $addresses = array();
 
-    protected $reconnect_count;
+    protected $sockets = array();
 
-    /**
-     * @var resource
-     */
-    protected $socket;
-
-    /**
-     * @var Buffer
-     */
-    protected $buffer;
-
-    /**
-     * @var Decoder
-     */
-    protected $decoder;
-
-    /**
-     * @var
-     */
-    protected $read_watcher;
-
-    /**
-     * @param $host
-     * @param int $port
-     * @param int $reconnect_count
-     * @throws ConnectionException
-     */
-    public function __construct($host, $port = 4150, $reconnect_count = 3)
+    public function __construct($addresses)
     {
-        $this->host = $host;
-        $this->port = $port;
-        $this->reconnect_count = $reconnect_count;
-        $this->reconnect();
-
-        $this->buffer = new Buffer();
-        $this->decoder = new Decoder();
+        foreach ($addresses as $address) {
+            $key = $address['ip'] . ':' . $address['port'];
+            $this->addresses[$key] = $address;
+        }
+        $this->refresh($addresses);
     }
 
-    /**
-     * reconnect to nsq server
-     * @throws ConnectionException
-     */
-    public function reconnect()
+    public function refresh($addresses)
     {
-        $count = 0;
-        while (true) {
-            try {
-                $this->socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-                if ($this->socket === FALSE) {
-                    $message = "socket_create() failed: reason: "
-                        . socket_strerror(socket_last_error());
-                    throw new ConnectionException($message);
-                }
+        $addresses_with_key = array();
+        foreach ($addresses as $address) {
+            $key = $address['ip'] . ':' . $address['port'];
+            $addresses_with_key[$key] = $address;
+        }
 
-                $connect_result = socket_connect($this->socket, $this->host, $this->port);
-                if ($connect_result === false) {
-                    $message = "socket_connect failed. reason:"
-                        . socket_strerror(socket_last_error());
-                    throw new ConnectionException($message);
-                }
-
-                $non_block = socket_set_nonblock($this->socket);
-                if ($non_block === false) {
-                    $message = "socket_set_nonblock faild. reason:"
-                        . socket_strerror(socket_last_error());
-                    throw new ConnectionException($message);
-                }
-
-                break;
-            } catch (\Exception $e) {
-                $count++;
-                echo $count . PHP_EOL;
-                if ($count > $this->reconnect_count) throw $e;
-
-                sleep(self::RECONNECT_WAIT_TIME * $count);
+        foreach ($addresses_with_key as $key => $address) {
+            if (array_key_exists($key, $this->sockets)) continue;
+            $socket = stream_socket_client("tcp://{$address['ip']}:{$address['port']}", $error_no, $error_str, 30);
+            if ($socket === false) {
+                throw new ConnectionException("stream_socket_client failed. reason:" . $error_str, $error_no);
             }
+            $this->sockets[$key] = $socket;
+        }
+
+        foreach ($this->sockets as $key => $socket) {
+            if (array_key_exists($key, $addresses_with_key)) continue;
+            fclose($socket);
+            unset($this->sockets[$key]);
         }
     }
 
-    /**
-     * @param $data
-     * @return bool
-     * @throws ConnectionException
-     */
-    public function write($data)
+    public function write($content)
     {
-        $write_result = socket_write($this->socket, $data, strlen($data));
-        if ($write_result === false) {
-            $message = "write to socket failed. reason:"
-                . socket_strerror(socket_last_error());
-            throw new ConnectionException($message);
-        }
 
-        return true;
-    }
-
-    /**
-     * @param $callback
-     * @return $this
-     * @throws ConnectionException
-     */
-    public function onMessage($callback)
-    {
-        if (!is_callable($callback)) {
-            throw new ConnectionException("params callback is not callable");
-        }
-
-        $this->read_watcher = new \EvIo($this->socket, \Ev::READ, function ($watcher, $reader) use ($callback) {
-            $frame = $this->decoder->readFame($this);
-            if ($frame === false) {
-                return false;
-            }
-            if ($frame->getType() == Decoder::FRAME_TYPE_ERROR) {
-                throw new RuntimeException("frame type error. type:" . $frame->getType());
-            }
-
-            return call_user_func($callback, $frame->getMessage());
-        });
-
-        return $this;
-    }
-
-    /**
-     * @param int $length
-     * @param bool|true $block
-     * @return bool
-     * @throws ConnectionException
-     */
-    public function read($length = 4, $block = true)
-    {
-        if ($block === false) {
-            socket_recv($this->socket, $data, $length, MSG_DONTWAIT);
-            $e_non_blocking = array(
-                11,/*EAGAIN or EWOULDBLOCK*/
-                115/*EINPROGRESS*/
-            );
-            // Caught EINPROGRESS, EAGAIN, or EWOULDBLOCK
-            if (in_array(socket_last_error(), $e_non_blocking)) {
-                return false;
-            }
-
-            return $data;
-        }
-
-        $ret = socket_recv($this->socket, $data, $length, MSG_WAITALL);
-        if ($ret === false) {
-            $message = "socket_recv failed. reason:"
-                . socket_strerror(socket_last_error());
-            throw new ConnectionException($message);
-        }
-
-        return $data;
-    }
-
-    public function equal($host, $port)
-    {
-        return $this->host == $host && $this->port == $port;
-    }
-
-    public function getSocket()
-    {
-        return $this->socket;
-    }
-
-    public function close()
-    {
-        socket_close($this->socket);
-    }
-
-    public function __destruct()
-    {
-        $this->close();
     }
 }
